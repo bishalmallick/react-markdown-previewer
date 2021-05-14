2772ed50451c2bf65077bc77eb8940fb9982c02e
bishalmallick <bishalmallick@gmail.com>
2021-05-15 02:27:42 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import  marked from 'marked';
 
-function App() {
-  return (
+const initialState = `
+# This is a heading 1
+## This is a heading 2
+
+This is a Paragraph with **bolded text**
+
+- list item 1
+- list item 2
+- list item 3
+
+[Visit my website](https://github.com/bishalmallick)
+
+This is an inline \`<div></div>\`
+
+This is a block of code
+
+\`\`\`
+  let x = 1;
+  let y = 2;
+  let z = x + y;
+\`\`\`
+
+This is an Image
+
+![React Logo](http://assets.stickpng.com/images/584830f5cef1014c0b5e4aa1.png)
+`;
+
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      text: initialState
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  render() {
+    const { text } = this.state;
+    const markdown = marked(text);
+    
+    return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <h1 className="header">MarkDown Previewer</h1>
+
+      <div className="container">
+
+        <div className="editor">
+          <h2>Editor</h2>
+          <textarea value={text} className="textarea" onChange={this.handleChange}/>
+        </div>
+
+        <div className="previewer">
+          <h2>Preview</h2>
+          <div className="preview" dangerouslySetInnerHTML={{__html: markdown}}></div>
+        </div>
+
+      </div>
+
+      <div className="footer">
+        <footer>
+          <p className="footer__text">
+          © 2021 - Website developed by <a href="https://github.com/bishalmallick" target="_blank" rel="noopener noreferrer"> Bishal Mallick</a>
+          </p>
+        </footer>
+      </div>
+
+    </div>)
+  }
 }
 
 export default App;
